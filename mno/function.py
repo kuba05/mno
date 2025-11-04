Find distance added, numerical_grad fixed, tests added

mno/function.py
@@ -38,6 +38,32 @@ class Function:
         self._dim_out = dim_out
         self._derivative = derivative
 
+    def __neg__(self) -> Function:
+        """Unary minus."""
+        return self * -1
+
+    def __add__(self, other: Function | float) -> Function:
+        """Addition."""
+        if isinstance(other, Function):
+            assert self.get_dim() == other.get_dim()
+            return Function(
+                lambda a: self(a) + other(a), dim_in=self._dim_in, dim_out=self._dim_out
+            )
+        return Function(
+            lambda a: self(a) + other, dim_in=self._dim_in, dim_out=self._dim_out
+        )
+
+    def __mul__(self, other: Function | float) -> Function:
+        """Multiplication."""
+        if isinstance(other, Function):
+            assert self.get_dim() == other.get_dim()
+            return Function(
+                lambda a: self(a) * other(a), dim_in=self._dim_in, dim_out=self._dim_out
+            )
+        return Function(
+            lambda a: self(a) * other, dim_in=self._dim_in, dim_out=self._dim_out
+        )
+
     def __call__(self, point: Vec) -> Vec:
         """Return function's value at a given point."""
         assert len(point) == self._dim_in, (
@@ -91,17 +117,17 @@ class Function:
                     np.array(
                         [
                             self._function(
-                                point + step * np.eye(len(point), dtype=TYPE)
+                                point + step * np.eye(len(point), dtype=TYPE)[i]
                             )
-                            for _ in range(len(point))
+                            for i in range(len(point))
                         ]
                     )
                     - np.array(
                         [
                             self._function(
-                                point - step * np.eye(len(point), dtype=TYPE)
+                                point - step * np.eye(len(point), dtype=TYPE)[i]
                             )
-                            for _ in range(len(point))
+                            for i in range(len(point))
                         ]
                     )
                 ).reshape(-1)

mno/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from mno.function import Function
 from mno.linesearch import GoldenSearch, TernarySearch
 from mno.my_types import array_to_vec
@@ -8,8 +10,73 @@ def main() -> None:
     """Run the whole thing."""
     x = (
         GoldenSearch()
+        .set_function(Function(lambda a: a[0] ** 2, dim_in=1, dim_out=1))
+        .set_stopping_condition(stopping_condition=SmallChangeCondition())
+        .set_interval(
+            point_a=array_to_vec([-4]),
+            point_b=array_to_vec([4]),
+        )
+    )
+    print(x.solve())
+    x.draw_picture()
+
+    x = (
+        GoldenSearch()
         .set_function(
-            Function(lambda a: ((a[0] + 2) ** 2) - a[1] ** 2, dim_in=2, dim_out=1)
+            Function(
+                lambda a: (a[0] - 2) ** 2 + 0.01 * (a[0] - 2) * a[0],
+                dim_in=1,
+                dim_out=1,
+            )
+        )
+        .set_stopping_condition(stopping_condition=SmallChangeCondition())
+        .set_interval(
+            point_a=array_to_vec([-4]),
+            point_b=array_to_vec([4]),
+        )
+    )
+    print(x.solve())
+    x.draw_picture()
+
+    x = (
+        GoldenSearch()
+        .set_function(
+            Function(
+                lambda a: -(a[0] - np.sin(a[0])) * np.e ** (a[0] ** 2),
+                dim_in=1,
+                dim_out=1,
+            )
+        )
+        .set_stopping_condition(stopping_condition=SmallChangeCondition())
+        .set_interval(
+            point_a=array_to_vec([-4]),
+            point_b=array_to_vec([4]),
+        )
+    )
+    print(x.solve())
+    x.draw_picture()
+
+    x = (
+        GoldenSearch()
+        .set_function(
+            Function(
+                lambda a: -a[0] * np.sin(a[0] ** 2),
+                dim_in=1,
+                dim_out=1,
+            )
+        )
+        .set_stopping_condition(stopping_condition=SmallChangeCondition())
+        .set_interval(
+            point_a=array_to_vec([-4]),
+            point_b=array_to_vec([4]),
+        )
+    )
+    print(x.solve())
+    x.draw_picture()
+    """x = (
+        GoldenSearch()
+        .set_function(
+            Function(lambda a: ((a[0] + 2) ** 4) - a[1] ** 2, dim_in=2, dim_out=1)
         )
         .set_stopping_condition(stopping_condition=SmallChangeCondition())
         .set_interval(
@@ -18,7 +85,7 @@ def main() -> None:
         )
     )
     print(x.solve())
-    x.draw_picture()
+    x.draw_picture()"""
 
 
 if __name__ == "__main__":

mno/my_types.py
@@ -1,20 +1,25 @@
 import numpy as np
 import numpy.typing as npt
 
-TYPE = float | np.floating
+TYPE = float
 Vec = npt.NDArray
 
 
-def float_to_vec(x: TYPE) -> Vec:
+def float_to_vec(x: TYPE | np.floating) -> Vec:
     """Convert float to Vec."""
-    return np.array([x])
+    return np.array([x], dtype=TYPE)
 
 
 def array_to_vec(x: list | tuple) -> Vec:
     """Convert array to Vec."""
-    return np.array(x)
+    return np.array(x, dtype=TYPE)
 
 
 def dot_prorduct(x: Vec, y: Vec) -> Vec:
     """Return the dot product of two vectors."""
     return np.array(x.dot(y))
+
+
+def norm(x: Vec) -> Vec:
+    """Return the square of the L2 norm."""
+    return dot_prorduct(x, x)